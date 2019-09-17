894de63e2616d832a469534192929fec1aa3de15
podsiadlo.pawel <podsiadlo.pawel@gmail.com>
2019-09-17 17:40:44 +0200

    Add project files.

---
+++ Exceptions/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //int a, b;
+                //a = 2;
+                //b = 0;
+                //Test test = new Test();
+                //test.X=0;
+                //Test.Name = "Roman";
+                //throw new Exception("Testowy błąd");
+                //int x = a / b;
+                var name = ConfigurationManager.AppSettings["name"];
+                Console.WriteLine(name);
+                Names names = Names.Krzysiek;
+                switch (names)
+                {
+                    case Names.Roman:
+                        break;
+                    case Names.Anna:
+                        break;
+                    case Names.Krzysiek:
+                        Console.WriteLine(Names.Krzysiek.ToString());
+                        Console.WriteLine((int)Names.Krzysiek);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch(DivideByZeroException dbze)
+            {
+                Console.WriteLine(dbze.Message);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Koniec programu");
+            }
+     
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exceptions/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class Test
+    {
+        public int X;
+        public static string Name;
+    }
+    public enum Names
+    {
+        Roman,
+        Anna=2,
+        Krzysiek=4
+    }
+}
